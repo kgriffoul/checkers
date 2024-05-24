d438436560b93d6c4341a771e71d95e8cb390cf5
kgriffoul <168074215+kgriffoul@users.noreply.github.com>
2024-05-24 11:57:52 +0200

    dessin plateau

--- src/application/Main.java
+++ src/application/Main.java
@@ -18,24 +18,32 @@ import javafx.scene.paint.Color;
 public class Main extends Application {
 	@Override
     public void start(Stage stage) {
-        final Image whitePiece = new Image(WHITE_PIECE);
-        final Image blackPiece = new Image(BLACK_PIECE);
-
 
         final Canvas canvas = new Canvas(640, 640);
         final GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.GOLD);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
+        
+        
         Board board = new Board(new Player("a"), new Player("b"));
-        for (Piece piece : board.getPieces()) {
-        	if (piece.getColor() == game.Color.WHITE) {
-        		gc.drawImage(whitePiece, piece.getX()*64, piece.getY()*64, 64, 64);
-        	} else {
-        		gc.drawImage(blackPiece, piece.getX()*64, piece.getY()*64, 64, 64);
-        	}
-        }
-        gc.drawImage(whitePiece, 0, 0, 64, 64);
+        drawBoard(gc, board);
+        
+        canvas.setOnMouseClicked(event -> {
+        	
+        	gc.clearRect(0, 0, 640, 640);
+        	drawBoard(gc, board);
+        	
+            int x = (int) event.getX() / 64;
+            int y = (int) event.getY() / 64;
+            System.out.printf("Click at %d %d%n", x, y);
+            if (board.isPieceAt(x, y)) {
+            	gc.drawImage(SELECT, x * 64, y * 64, 64, 64);
+        		for(String pos : board.getPieceAt(x, y).getMoveList()) {
+        			
+        		}
+            }
+        });
 
         stage.setScene(new Scene(new Group(canvas)));
         stage.show();
@@ -43,7 +51,20 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-    private static final String WHITE_PIECE = "resources/white_piece.png";
-    private static final String BLACK_PIECE = "resources/black_piece.png";
+    
+    public static void drawBoard(GraphicsContext gc, Board board) {
+    	gc.drawImage(BOARD, 0, 0, 640, 640);
+    	for (Piece piece : board.getPieces()) {
+        	if (piece.getColor() == game.Color.WHITE) {
+        		gc.drawImage(WHITE_PIECE, piece.getX()*64, piece.getY()*64, 64, 64);
+        	} else {
+        		gc.drawImage(BLACK_PIECE, piece.getX()*64, piece.getY()*64, 64, 64);
+        	}
+        }
+    }
+    
+    private static final Image BOARD = new Image("resources/board.png");
+    private static final Image SELECT = new Image("resources/select.png");
+    private static final Image WHITE_PIECE = new Image("resources/white_piece.png");
+    private static final Image BLACK_PIECE = new Image("resources/black_piece.png");
 }
